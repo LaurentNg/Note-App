cc13fefe7a4f742c67d8b434d4c8b343129fb8f1
laurent.nguyen <laurent.nguyen@sentiom.com>
2024-01-27 20:21:52 -0500

    added requiered body fields

--- internal/models/decrypt.go
+++ internal/models/decrypt.go
@@ -1,6 +1,6 @@
 package models
 
 type Decrypt struct {
-    EncryptedText string  `json:"text"`
-    Passphrase    string  `json:"passphrase"`
+    EncryptedText string  `json:"text" binding:"required"`
+    Passphrase    string  `json:"passphrase" binding:"required"`
 }
--- internal/models/encrypt.go
+++ internal/models/encrypt.go
@@ -1,6 +1,6 @@
 package models
 
 type Encrypt struct {
-    Text     	string  `json:"text"`
-    Passphrase  string  `json:"passphrase"`
+    Text     	string  `json:"text" binding:"required"`
+    Passphrase  string  `json:"passphrase" binding:"required"`
 }
