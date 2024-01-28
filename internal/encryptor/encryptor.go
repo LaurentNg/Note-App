f20458b96e316da9af6f4a79fa40d08fbf1b65ec
laurent.nguyen <laurent.nguyen@sentiom.com>
2024-01-27 19:58:22 -0500

    update encryption service

--- internal/encryptor/encryptor.go
+++ internal/encryptor/encryptor.go
@@ -2,8 +2,12 @@ package encryptor
 
 import (
 	"crypto/aes"
+	"crypto/sha256"
 	"encoding/hex"
 	"errors"
+	"io"
+
+	"golang.org/x/crypto/hkdf"
 )
 
 // https://www.developer.com/languages/cryptography-in-go/
@@ -12,19 +16,31 @@ var (
 	ErrWrongPassphrase = errors.New("The provided passphrase is incorrect")
 )
 
-func Encrypt(key string, text string) (string, error) {
-	c, err := aes.NewCipher([]byte(key))
+func Encrypt(passphrase string, text string) (string, error) {
+	key, err := generate_key(passphrase)
 	if err != nil {
 		return "", err
 	}
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
 	textByte := make([]byte, len(text))
 	c.Encrypt(textByte, []byte(text))
 	return hex.EncodeToString(textByte), nil
 }
 
-func Decrypt(key string, text string) (string, error) {
+func Decrypt(passphrase string, text string) (string, error) {
 	txt, _ := hex.DecodeString(text)
-	c, err := aes.NewCipher([]byte(key))
+
+	key, err := generate_key(passphrase)
+	if err != nil {
+		return "", err
+	}
+
+	c, err := aes.NewCipher(key)
 	if err != nil {
 		return "", ErrWrongPassphrase
 	}
@@ -32,4 +48,17 @@ func Decrypt(key string, text string) (string, error) {
 	c.Decrypt(textByte, []byte(txt))
 
 	return string(textByte), nil
+}
+
+func generate_key(passphrase string) ([]byte, error) {
+	hkdfSalt := make([]byte, 32)
+	info := []byte("AES Key Derivation")
+	key := make([]byte, 32)
+
+	r := hkdf.New(sha256.New, []byte(passphrase), hkdfSalt, info)
+	if _, err := io.ReadFull(r, key); err != nil {
+		return nil, err
+	}
+
+	return key, nil
 }
