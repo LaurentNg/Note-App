abbe86c2531056c6814bc2f7e5046535051b2230
laurent.nguyen <laurent.nguyen@sentiom.com>
2024-01-28 21:19:13 -0500

    update server to use cors

--- backend/internal/api/server.go
+++ backend/internal/api/server.go
@@ -3,11 +3,13 @@ package server
 import (
 	"Note-App/internal/api/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
 	router := gin.Default()
+	router.Use(cors.Default())
 	routes.Encryptor_routes(&router.RouterGroup)
     router.Run("localhost:8080")
 }
